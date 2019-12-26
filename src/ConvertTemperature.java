8073f9a89731fb836bddbcac29059c80782a84ab
matbiec1102 <bietttuot505@gmail.com>
2019-12-26 16:30:18 +0700

    done

---
+++ src/ConvertTemperature.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class ConvertTemperature {
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+        int choice;
+
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+}
